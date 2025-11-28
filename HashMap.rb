f519f82359a204e7a5849fef3eaae890fd6e1dd4
DaveBett <davide.betterini@gmail.com>
2025-11-28 15:57:04 +0000

    Implemented #hash method for HashMap class

--- HashMap.rb
+++ HashMap.rb
@@ -3,4 +3,14 @@ class HashMap
     @load_factor = 0,75
     @capacity = 16
   )
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+        
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+        
+    hash_code
+  end
+  
 end
