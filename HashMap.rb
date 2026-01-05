0e1ed4ec645209006407f8666a23a123e6d2abe5
DaveBett <davide.betterini@gmail.com>
2026-01-05 18:53:39 +0000

    Implemented #set method to HashMap class

--- HashMap.rb
+++ HashMap.rb
@@ -13,4 +13,25 @@ class HashMap
     hash_code
   end
   
+    def set(key, value)
+    hash_code = hash(key)
+    bucket_index = hash_code % @capacity
+    bucket = @buckets[bucket_index]
+
+    found = false
+
+    bucket.each  do |pair| 
+      if pair[0] == key
+        pair[1] = value
+        found = true
+      end
+    end
+    unless found
+      bucket << [key,value]
+    end
+    
+    if length > (@capacity * @load_factor)
+        resize
+    end
+  end
 end
